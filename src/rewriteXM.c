46a9303d3d61c5024954b3629601e1191c754b22
torph <73564623+Torphedo@users.noreply.github.com>
2026-07-08 12:35:06 -0400

    Fix major clipping issues with XM

--- src/rewriteXM.c
+++ src/rewriteXM.c
@@ -10,28 +10,61 @@
 #include "VirtualIO.h"
 
 void pcmDeltaDecode8(const int8_t* sample, int8_t* pcmOut, uint32_t sampleCount) {
-    int8_t old = 0, new;
+    int16_t amp;
     for (uint32_t i = 0; i < sampleCount; i++) {
-        new = sample[i] + old;
-        pcmOut[i] = new;
-        old = new;
+        amp += sample[i];
+        // The sample may go beyond the 8-bit boundary, just grab the signed 8-bit portion
+        pcmOut[i] = (int8_t)amp;
     }
 }
 
 void pcmDeltaDecode16(const int16_t* sample, int16_t* pcmOut, uint32_t sampleCount) {
-    int16_t old = 0, new;
+    int32_t amp;
     for (uint32_t i = 0; i < sampleCount; i++) {
-        new = sample[i] + old;
-        pcmOut[i] = new;
+        amp += sample[i];
+        // The sample may go beyond the 16-bit boundary, just grab the signed 16-bit portion
+        pcmOut[i] = (int16_t)amp;
+    }
+}
+
+// Decode 8-bit delta-coded samples, and convert to 16-bit PCM
+void pcmDelta8to16(const int8_t* sample, int16_t* pcmOut, uint32_t sampleCount) {
+    int16_t amp = 0;
+    for( uint32_t i = 0; i < sampleCount; i++ ) {
+        amp += sample[i]; // Calculate next sample value
+
+        // The sample may go beyond the 8-bit boundary, just grab the signed
+        // 8-bit portion but at 16-bit magnitude.
+        pcmOut[i] = amp << 8;
+    }
+}
+
+void pcmDeltaEncode16to8(const int16_t* pcmIn, int8_t* out, uint32_t sampleCount) {
+    int8_t old = 0, new;
+    for (uint32_t i = 0; i < sampleCount; i++) {
+        new = pcmIn[i] >> 8; // Convert to 8-bit magnitude
+        const int8_t diff = new - old;
+
+        const int8_t result = old + diff;
+        // Despite the diff value overflowing, the truncated 8-bit diff will end
+        // up reaching the target sample value via over/underflow
+        assert(result == new);
+
+        out[i] = diff;
         old = new;
     }
 }
+
 
 void pcmDeltaEncode8(const int8_t* pcmIn, int8_t* out, uint32_t sampleCount) {
     int8_t old = 0, new;
     for (uint32_t i = 0; i < sampleCount; i++) {
         new = pcmIn[i];
-        const int8_t diff = new - old;
+        int8_t diff = new - old;
+        const int8_t result = old + diff;
+        // Despite the diff value overflowing, the truncated 8-bit diff will end
+        // up reaching the target sample value via over/underflow
+        assert(result == new);
         out[i] = diff;
         old = new;
     }
@@ -91,14 +124,8 @@ bool writeOpusSampleXM(VirtualIO* io, const void* data, uint32_t size, bool is16
         return false;
     }
     if (!is16Bit) {
-        // Convert to 16-bit then convert to PCM in-place
-        void* pcm8 = malloc(sampleCount);
-        pcmDeltaDecode8(data, pcm8, sampleCount);
-        pcm8to16(pcm8, pcmData, sampleCount);
-        free(pcm8);
-
-        // pcm8to16(data, pcmData, sampleCount);
-        // pcmDeltaDecode16(pcmData, pcmData, sampleCount);
+        // Convert 8-bit delta-coded PCM to 16-bit raw PCM
+        pcmDelta8to16(data, pcmData, sampleCount);
     } else {
         // Just convert to PCM
         pcmDeltaDecode16(data, pcmData, sampleCount);
@@ -162,8 +189,7 @@ bool decodeOpusSampleXM(VirtualIO* io, const void* data, uint32_t size, bool is1
 
     // Delta encode in-place
     if (!is16Bit) {
-        pcm16to8(buf, buf, sampleCount); // Convert to 8-bit in-place
-        pcmDeltaEncode8(buf, buf, sampleCount);
+        pcmDeltaEncode16to8(buf, buf, sampleCount);
         bufSize /= 2;
     } else {
         pcmDeltaEncode16(buf, buf, sampleCount);
